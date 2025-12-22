b599cb4b10dbfa7fdd9052aae36d17371bc9fd23
d4rk-h <herreragp334@gmail.com>
2025-12-22 12:44:37 +0000

    feat: finish the request script formatter

--- qparser/_format_script.py
+++ qparser/_format_script.py
@@ -1,7 +1,7 @@
 import json
 
 def get_multiline_input():
-    print("Paste your code (Ctrl+D when done):")
+    print("\nPaste your code (Ctrl+D when done):")
     lines = []
     try:
         while True:
@@ -12,4 +12,15 @@ def get_multiline_input():
 
 code = get_multiline_input()
 escaped_code = json.dumps(code)
-print(f'\nPaste this into your curl command:\n{escaped_code}')
+print(f'\n\nPaste this into your curl command:\n{escaped_code}')
+
+print("\n Do you want the full request formatted? (y/n): ", end="")
+if input().lower() == 'y':
+    script_json = escaped_code
+    script_type = "\"QASM\"" if "QASM" in escaped_code else "\"QISKIT\""
+    port = int(input("Introduce a port if other is preferred (default 8080) else press Enter: ") or 8080)
+    full_json_request = "\n\n" + "curl -X POST http://localhost:%d/api/parse \ "+"""\n  -H "Content-Type: application/json" \ """+"""\n  -d '{"""+"""\n      "script:" %s,"""+"""\n      "scriptType": %s"""+"""\n   }'"""
+    print(f'\nFull request:\n{full_json_request % (port, script_json, script_type)}')
+    print(f"\n\nPaste this into your terminal to run the request. Ensure your server is running on port {port}.")
+else:
+    print("\n   Exiting without full request.")
