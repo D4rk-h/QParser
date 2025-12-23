52cdd47f9c53586c84ac5f73ea2b934fc8aaf8cc
d4rk-h <herreragp334@gmail.com>
2025-12-23 20:04:06 +0000

    VERSION 1.0.0
    refactor:
    final naming changes.

--- qparser/src/main/java/Main.java
+++ qparser/src/main/java/Main.java
@@ -4,7 +4,7 @@ import infrastructure.web.RouteRegistry;
 import io.javalin.Javalin;
 
 public class Main {
-    private static final String DEFAULT_PORT = System.getenv("DEFAULT_PORT");
+    private static final String DEFAULT_PORT = System.getenv("PORT");
 
     public static void main(String[] args) {
         int port = getPort(args);
@@ -18,7 +18,7 @@ public class Main {
     }
 
     private static int getPort(String[] args) {
-        String envPort = System.getenv("DEFAULT_PORT");
+        String envPort = System.getenv("PORT");
         if (args.length > 0) {
             try {return Integer.parseInt(args[0]);}
             catch (NumberFormatException e) {System.err.println("Invalid port, using default: " + DEFAULT_PORT);}
